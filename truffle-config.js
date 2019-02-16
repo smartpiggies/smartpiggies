999c290efcedb61f95fee5cab7a17ce1e0794826
tobyjaguar <talgya@gmail.com>
2019-02-16 02:06:05 -0700

    push drizzle default

---
+++ truffle-config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+  migrations_directory: "./migrations",
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      gas:6000000,
+      gasPrice: 1100000000,
+      network_id: "*" // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.2",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 500
+        }
+      }
+    }
+  }
+};

cf3722281f3e00ff7d789cc8cc778268f0a52986
tobyjaguar <talgya@gmail.com>
2019-04-13 18:57:07 -0400

    update app for new deployed SmartPiggies contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -16,6 +16,7 @@ var oraclePrice = 27000
 module.exports = function(deployer) {
   deployer.deploy(SmartPiggies, {gas: 8000000, gasPrice: 1100000000, overwrite: false});
   deployer.deploy(StableToken, {gas: 3000000, gasPrice: 1100000000, overwrite: false});
+  deployer.deploy(StableTokenFaucet, {gas: 3000000, gasPrice: 1100000000, overwrite: false});
   deployer.deploy(TestnetLINK, {gas: 3000000, gasPrice: 1100000000, overwrite: false})
   .then(() => {
     return deployer.deploy(ResolverSelfReturn,
